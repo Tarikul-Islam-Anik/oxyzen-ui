2a37bf4102be550ff0f10cf328593b1a77d23158
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-25 12:40:15 +0600

    Add theme configuration file

---
+++ theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+  logo: <span style={{ fontWeight: "bold" }}>OxyZen UI</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  head: (
+    <>
+      <title>OxyZen UI</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="OxyZen UI" />
+      <meta property="og:description" content="Simple UI library for React" />
+    </>
+  ),
+};
